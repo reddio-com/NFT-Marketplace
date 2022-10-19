2f9458c3b2efd97af68b6001e65505f19002a955
KieSun <zx597813039@gmail.com>
2022-10-19 11:45:32 +0800

    feat: contract address

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,10 @@
-const ERC20Address = '0x57F3560B6793DcC2cb274c39E8b8EBa1dd18A086';
-const ERC721Address = '0x941661bd1134dc7cc3d107bf006b8631f6e65ad5';
+import { isVercel } from '@/utils/config';
+
+const ERC20Address = !isVercel
+  ? '0x57F3560B6793DcC2cb274c39E8b8EBa1dd18A086'
+  : '0xB8c77482e45F1F44dE1745F52C74426C631bDD52';
+const ERC721Address = !isVercel
+  ? '0x941661bd1134dc7cc3d107bf006b8631f6e65ad5'
+  : '0x316442f06d97fc9864ff32f84b9fcaecd71f049d';
 
 export { ERC721Address, ERC20Address };
