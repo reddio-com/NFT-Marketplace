335b6214bd45423ee58563e5de846880c2bd9b0d
KieSun <zx597813039@gmail.com>
2022-11-23 17:20:35 +0800

    chore: update pkg

--- src/components/nft/nftList.tsx
+++ src/components/nft/nftList.tsx
@@ -54,7 +54,6 @@ const NFTList = () => {
                 item.contract_address.toLowerCase() === address.toLowerCase(),
             )
             .map((item) => item.token_id);
-          console.log(ids);
           // @ts-ignore
           if (ids.length) setBaseUri(data.data.list[0].base_uri);
         } else {
