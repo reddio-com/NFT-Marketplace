d92e2aae5bc4941f3b01f97961b1c0a1828e3172
KieSun <zx597813039@gmail.com>
2023-03-14 14:13:35 +0800

    feat: update reddio init

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,15 +1,13 @@
-import { ethers } from 'ethers';
 import { Reddio } from '@reddio.com/js';
 
 const isVercel = process.env.IS_VERCEL !== '1';
 
 let reddio: Reddio;
-const initReddio = () => {
+const initReddio = (client: any) => {
   if (typeof window !== 'undefined' && !reddio) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
     reddio = new Reddio({
-      provider,
       env: isVercel ? 'main' : 'test',
+      wagmiClient: client,
     });
   }
 };
