fbc107a73661f46594907b595359785318f99cb9
KieSun <zx597813039@gmail.com>
2023-03-14 14:13:19 +0800

    feat: utils use wagmi

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,38 +1,27 @@
-import { ethers } from 'ethers';
-import erc20Abi from '@/abi/Erc20.abi.json';
+import { getAccount, readContract, fetchBalance } from '@wagmi/core'
 import erc721Abi from '@/abi/Erc721.abi.json';
 import { ERC721Address } from '@/utils/common';
 
-const initProviderAndSigner = async () => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = await provider.getSigner();
-  return { provider, signer };
-};
-
 const getEthAddress = async () => {
-  const { signer } = await initProviderAndSigner();
-  return signer.getAddress();
+  return getAccount().address
 };
 
 const getContractBalance = async (contractAddress: string) => {
-  const { provider, signer } = await initProviderAndSigner();
-  const code = await provider.getCode(contractAddress);
-  if (code) {
-    const contract = new ethers.Contract(contractAddress, erc20Abi, signer);
-    const balance = await contract.balanceOf(await signer.getAddress());
-    return ethers.utils.formatEther(balance.toString());
-  }
+  const balance = await fetchBalance({ address: (await getEthAddress())!, token: contractAddress as `0x` })
+  return balance.formatted
 };
 
 const getTokenURI = async (tokenId: number) => {
-  const { signer } = await initProviderAndSigner();
-  const contract = new ethers.Contract(ERC721Address, erc721Abi, signer);
-  return await contract.tokenURI(tokenId);
+  return readContract({
+    address: ERC721Address as `0x${string}`,
+    abi: erc721Abi,
+    functionName: 'tokenURI',
+    args: [tokenId],
+  })
 };
 
 export {
   getEthAddress,
-  initProviderAndSigner,
   getContractBalance,
   getTokenURI,
 };
